2e4d75acaa7b8772a42d4f294730c45a1154bfe5
Dawid Poznański <dawid.poznanski@outlook.com>
2021-09-18 12:38:42 +0200

    add react-router transitions, new home page layout

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -94,6 +94,18 @@ const lists = [
     description: 'Interesting things I want to check out!',
     image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
   },
+  {
+    id: 'list-2',
+    title: 'Things to do <sup>now!</sup>',
+    description: 'Interesting things I want to do!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
+  {
+    id: 'list-3',
+    title: 'Things to do <sup>not necessary!</sup>',
+    description: 'Bucket List!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
 ];
 
 const columns = [
